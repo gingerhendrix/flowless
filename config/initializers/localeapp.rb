b72437d23bcd6bdf2ccbb2a810996fb0fe18c8ef
Omniless <alex@omniless.com>
2013-12-03 18:23:19 -0500

    removing the hack for localeapp and adding the api key to drone

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,7 +1,7 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key                = ENV['LOCALE_APP_KEY'] || 'no_key_to_avoid_exception'
+  config.api_key                = ENV['LOCALE_APP_KEY']
   config.polling_environments   = [ :production ]
   config.poll_interval          = 60
   config.reloading_environments = [:development, :production]
